4e8a726a393e82237a5427fee498233a2982f043
monthebrice2000 <monthedjeumoubrice2000@gmail.com>
2022-06-15 13:59:26 +0200

    remove orange media with his sign video

--- src/js/custom/accessible_player.js
+++ src/js/custom/accessible_player.js
@@ -1,12 +1,12 @@
 let repeat_call = setInterval( function(){
-  if( typeof videojs !== "undefined" ){
+  if( !!videojs ){
     start();
     clearInterval( repeat_call );
   }
 }, 300)
-
+let accesPlayer ;
 const start = () => {
-  const accesPlayer = videojs("#video_access");
+  accesPlayer = videojs("#video_access");
   //console.log( accesPlayer )
 
   /**
@@ -26,24 +26,24 @@ const start = () => {
   srcElement.attr("src", srcElement.data("signSrc") );
   accesPlayer.signVideo.attr("src", srcElement.data("signSrc") );
 
-  $(videoAccess).on("playing pause seeked timeupdate ended", function(e) {
+  $(videoAccess).on("playing pause seeked timeupdate ended seeking", async function(e) {
     switch ( e.type ) {
       case "playing":
-        accesPlayer.signVideo.get(0).play();
+        accesPlayer.signVideo.get(0).play()
         break;
       case "pause":
         accesPlayer.signVideo.get(0).pause();
         break;
-      /*case "ended":
-        accesPlayer.signVideo.get(0).pause();
-        break;*/
-
       case "seeked":
-        accesPlayer.signVideo.get(0).currentTime = videoAccess.currentTime;
         break;
       case "timeupdate":
-        console.log( { video : videoAccess.currentTime, signvideo: accesPlayer.signVideo.get(0).currentTime })
+        console.log( {  ev: "timeupdate", video : videoAccess.currentTime, signvideo: accesPlayer.signVideo.get(0).currentTime })
         break;
+      case "seeking":
+        accesPlayer.signVideo.get(0).currentTime = videoAccess.currentTime;
+        console.log( {  ev: "seeking", video : videoAccess.currentTime, signvideo: accesPlayer.signVideo.get(0).currentTime })
+        break;
+
     }
   })
 }
